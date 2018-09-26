fa617d0385a5218f01a53d5a80d86b94e8db6234
unknown <Administrador@SAN-31008H2.usersad.everis.int>
2018-09-26 16:50:23 -0300

    adding different backgrounds based on weather description

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,6 +13,7 @@ export class AppComponent {
   city: string;
   errorMessage: string;
   weather: ICityWeather;
+  backgroundImage: string = 'src/assets/images/landscape.jpg';
 
   constructor(private weatherService: WeatherService){
 
@@ -23,6 +24,31 @@ export class AppComponent {
       weather => {
           this.weather = weather;
           this.weather.main.temp = this.weather.main.temp - 273.15;
+
+          if(this.weather.weather[0].main == 'Clear'){
+            this.backgroundImage = 'src/assets/images/clear.jpg';
+          }
+
+          else if(this.weather.weather[0].main == 'Fog'){
+            this.backgroundImage = 'src/assets/images/fog.jpg';
+          }
+
+          else if(this.weather.weather[0].description == 'few clouds'){
+            this.backgroundImage = 'src/assets/images/fewClouds.jpg';
+          }
+
+          else if(this.weather.weather[0].description == 'scattered clouds'){
+            this.backgroundImage = 'src/assets/images/scatteredClouds.jpg';
+          }
+
+          else if(this.weather.weather[0].description == 'shower rain'){
+            this.backgroundImage = 'src/assets/images/showerRain.jpg';
+          }
+
+          else if(this.weather.weather[0].description == 'clear sky'){
+            this.backgroundImage = 'src/assets/images/clearSky.jpg';
+          }
+
       },
       error => this.errorMessage = <any>error
     );
